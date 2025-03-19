3cb525a6d5e6944e24eb5e19680e687b382e6089
Ethan <jec0489@mavs.uta.edu>
2025-03-19 15:49:33 -0500

    More Accurate Motor Functions

--- dcTest.py
+++ dcTest.py
@@ -4,13 +4,27 @@ import time
 
 def main():
     try:
+        '''
         motor = MotorClass.DCMotor(38, 40)
         motor.move_motor(-20, 5.0)
+        '''
+        pin1 = 38
+        pin2 = 40
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(pin1, GPIO.OUT)
+        GPIO.setup(pin2, GPIO.OUT)
+        in1 = GPIO.PWM(pin1, 50)
+        in2 = GPIO.PWM(pin2, 50)
+        in1.start(0)
+        in2.start(0)
 
-
+        in1.ChangeDutyCycle(30)
+        print('Motor is moving\n')
+        time.sleep(5)
+        print("Motor has stopped\n")
     finally:
         GPIO.cleanup()
-        print("Pins are clean")
+        print("Pins are clean\n")
 
 if __name__ == '__main__':
     main()
