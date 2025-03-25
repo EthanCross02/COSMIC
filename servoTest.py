cc518438c924eee5100e9fb5e58665a994a5f152
Ethan <jec0489@mavs.uta.edu>
2025-03-25 13:18:52 -0500

    More Accurate Motor Functions

--- servoTest.py
+++ servoTest.py
@@ -4,12 +4,18 @@ import RPi.GPIO as GPIO
 
 def main():
     try:
-        '''
-        servo = MotorClass.ServoMotor(40)
-        servo.change_pos('CENTER')
-        print("Servo Should have moved")
-        '''
 
+        servo = MotorClass.ServoMotor(40)
+        servo.change_pos(1)
+        print("Minimum")
+        time.sleep(3)
+        servo.change_pos(100)
+        print("Maximum")
+        time.sleep(3)
+        servo.change_pos(50)
+        print("Neutral")
+
+        '''
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(40, GPIO.OUT)
         servo = GPIO.PWM(40, 100)
@@ -23,6 +29,7 @@ def main():
         print("Servo Should have moved")
         time.sleep(5)
         servo.stop()
+        '''
     finally:
         GPIO.cleanup()
         print('Pins are cleaned')
