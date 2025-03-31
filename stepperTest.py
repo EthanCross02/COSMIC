c587a0a941782f11e97aa80c48f4f513dcd26580
Ethan <jec0489@mavs.uta.edu>
2025-03-31 13:44:16 -0500

    More Accurate Motor Functions

--- stepperTest.py
+++ stepperTest.py
@@ -10,9 +10,9 @@ def main():
     try:
 
         stepper = MotorClass.Stepper(38, 40)
-        stepper.move_motor(800)
+        stepper.move_motor(100)
         time.sleep(1)
-        stepper.move_motor(-800)
+        stepper.move_motor(-100)
     finally:
         GPIO.cleanup()
         print("Pins are clean")
